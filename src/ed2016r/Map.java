0a1815d1fbfe29f3f1459c9bc6305af771a61718
vsctiago <vsctiago@yahoo.com>
2016-02-03 18:00:08 +0000

    Cenas

--- src/ed2016r/Map.java
+++ src/ed2016r/Map.java
@@ -23,6 +23,34 @@ public class Map extends Graph<Cidade> implements MapADT<Cidade> {
         this.wAdjMatrix = new ArrayUnorderedList[super.DEFAULT_CAPACITY][super.DEFAULT_CAPACITY];
     }
     
+    /**
+     * Verifica se index é válido
+     * @param index
+     * @return true se index for válido
+     */
+    @Override
+    public boolean indexIsValid(int index) {
+        return index >= 0 && index < this.wAdjMatrix.length;
+    }
+    
+    /**
+     * Adiciona um caminho entre duas cidades
+     * @param index1 index da cidade 
+     * @param index2 index da cidade destino
+     * @param alt 
+     */
+    public void addEdge (int index1, int index2, Alternativa alt)
+   {
+      if (indexIsValid(index1) && indexIsValid(index2))
+      {
+         wAdjMatrix[index1][index2].addRear(alt);
+      }
+   }
+    
+    /**
+     * Adiciona uma cidade
+     * @param vertex cidade
+     */
     @Override
     public void addVertex (Cidade vertex)
    {
@@ -34,8 +62,29 @@ public class Map extends Graph<Cidade> implements MapADT<Cidade> {
       {
          wAdjMatrix[numVertices][i] = new ArrayUnorderedList<>();
          wAdjMatrix[i][numVertices] = new ArrayUnorderedList<>();
+         adjMatrix[numVertices][i] = false;
+         adjMatrix[i][numVertices] = false;
       }      
       numVertices++;
+   }
+    
+    public void removeVertex (int index)
+   {
+      if (indexIsValid(index))
+      {
+         numVertices--;
+
+         for (int i = index; i < numVertices; i++)
+            vertices[i] = vertices[i+1];
+
+         for (int i = index; i < numVertices; i++)
+            for (int j = 0; j <= numVertices; j++)
+               wAdjMatrix[i][j] = wAdjMatrix[i+1][j];
+
+         for (int i = index; i < numVertices; i++)
+            for (int j = 0; j < numVertices; j++)
+               wAdjMatrix[j][i] = wAdjMatrix[j][i+1];
+      }
    }
     
     @Override
