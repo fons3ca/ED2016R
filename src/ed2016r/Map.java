110318926b2dce93f6c74ce95577c45a59c4c7e1
vsctiago <vsctiago@yahoo.com>
2016-02-03 18:28:17 +0000

    AddEdge,

--- src/ed2016r/Map.java
+++ src/ed2016r/Map.java
@@ -16,85 +16,106 @@ import java.util.Iterator;
 public class Map extends Graph<Cidade> implements MapADT<Cidade> {
 
     private ArrayUnorderedList<Alternativa>[][] wAdjMatrix;
-    
-    
+
     public Map() {
         super();
         this.wAdjMatrix = new ArrayUnorderedList[super.DEFAULT_CAPACITY][super.DEFAULT_CAPACITY];
     }
-    
-    /**
-     * Verifica se index é válido
-     * @param index
-     * @return true se index for válido
-     */
-    @Override
-    public boolean indexIsValid(int index) {
-        return index >= 0 && index < this.wAdjMatrix.length;
-    }
-    
+
     /**
      * Adiciona um caminho entre duas cidades
-     * @param index1 index da cidade 
+     *
+     * @param index1 index da cidade
      * @param index2 index da cidade destino
+     * @param alt
+     */
+    public void addEdge(int index1, int index2, Alternativa alt) {
+        if (indexIsValid(index1) && indexIsValid(index2)) {
+            wAdjMatrix[index1][index2].addRear(alt);
+            wAdjMatrix[index2][index1].addRear(alt);
+            adjMatrix[index1][index2] = true;
+            adjMatrix[index2][index1] = true;
+        }
+    }
+
+    /**
+     * Se existe estes vertices envia para AddEdge os indexs
+     * @param vertex1
+     * @param vertex2
      * @param alt 
      */
-    public void addEdge (int index1, int index2, Alternativa alt)
-   {
-      if (indexIsValid(index1) && indexIsValid(index2))
-      {
-         wAdjMatrix[index1][index2].addRear(alt);
-      }
-   }
-    
+    @Override
+    public void addEdge(Cidade vertex1, Cidade vertex2, Alternativa alt) {
+        addEdge(getIndex(vertex1), getIndex(vertex2), alt);
+    }
+
     /**
      * Adiciona uma cidade
+     *
      * @param vertex cidade
      */
     @Override
-    public void addVertex (Cidade vertex)
-   {
-      if (numVertices == vertices.length)
-         expandCapacity();
+    public void addVertex(Cidade vertex) {
+        if (numVertices == vertices.length) {
+            expandCapacity();
+        }
 
-      vertices[numVertices] = vertex;
-      for (int i = 0; i <= numVertices; i++)
-      {
-         wAdjMatrix[numVertices][i] = new ArrayUnorderedList<>();
-         wAdjMatrix[i][numVertices] = new ArrayUnorderedList<>();
-         adjMatrix[numVertices][i] = false;
-         adjMatrix[i][numVertices] = false;
-      }      
-      numVertices++;
-   }
-    
-    public void removeVertex (int index)
-   {
-      if (indexIsValid(index))
-      {
-         numVertices--;
+        vertices[numVertices] = vertex;
+        for (int i = 0; i <= numVertices; i++) {
+            wAdjMatrix[numVertices][i] = new ArrayUnorderedList<>();
+            wAdjMatrix[i][numVertices] = new ArrayUnorderedList<>();
+            adjMatrix[numVertices][i] = false;
+            adjMatrix[i][numVertices] = false;
+        }
+        numVertices++;
+    }
 
-         for (int i = index; i < numVertices; i++)
-            vertices[i] = vertices[i+1];
+    /**
+     * Remove um vertex (Cidade)
+     *
+     * @param index
+     */
+    public void removeVertex(int index) {
+        if (indexIsValid(index)) {
+            numVertices--;
 
-         for (int i = index; i < numVertices; i++)
-            for (int j = 0; j <= numVertices; j++)
-               wAdjMatrix[i][j] = wAdjMatrix[i+1][j];
+            for (int i = index; i < numVertices; i++) {
+                vertices[i] = vertices[i + 1];
+            }
 
-         for (int i = index; i < numVertices; i++)
-            for (int j = 0; j < numVertices; j++)
-               wAdjMatrix[j][i] = wAdjMatrix[j][i+1];
-      }
-   }
-    
+            for (int i = index; i < numVertices; i++) {
+                for (int j = 0; j <= numVertices; j++) {
+                    wAdjMatrix[i][j] = wAdjMatrix[i + 1][j];
+                    adjMatrix[i][j] = adjMatrix[i + 1][j];
+                }
+            }
+            for (int i = index; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    wAdjMatrix[j][i] = wAdjMatrix[j][i + 1];
+                    adjMatrix[i][j] = adjMatrix[i + 1][j];
+                }
+            }
+        }
+    }
+
+    /**
+     * Procura o vertex (Cidade) e se existir chama removeVertex(i)
+     *
+     * @param vertex
+     */
     @Override
-    public void addEdge(Cidade vertex1, Cidade vertex2, Alternativa weight) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void removeVertex(Cidade vertex) {
+        for (int i = 0; i < numVertices; i++) {
+            if (vertex.equals(vertices[i])) {
+                removeVertex(i);
+                return;
+            }
+        }
     }
 
     @Override
     public double shortestPathWeight(Cidade vertex1, Cidade vertex2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
