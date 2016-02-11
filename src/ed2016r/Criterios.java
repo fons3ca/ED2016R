80511cb9ada52f72fd8c7d6fa85ac198e0cf9adb
vsctiago <vsctiago@yahoo.com>
2016-02-11 17:20:59 +0000

    Class criterios

---
+++ src/ed2016r/Criterios.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed2016r;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class Criterios {
+    private double duracaoTotal;
+    private double custoMaximo;
+    private double perdasCombate;
+    private double numeroCombates;
+
+    public Criterios(double duracaoTotal, double custoMaximo, double perdasCombate, double numeroCombates) {
+        this.duracaoTotal = duracaoTotal;
+        this.custoMaximo = custoMaximo;
+        this.perdasCombate = perdasCombate;
+        this.numeroCombates = numeroCombates;
+    }
+
+    public double getDuracaoTotal() {
+        return duracaoTotal;
+    }
+
+    public void setDuracaoTotal(double duracaoTotal) {
+        this.duracaoTotal = duracaoTotal;
+    }
+
+    public double getCustoMaximo() {
+        return custoMaximo;
+    }
+
+    public void setCustoMaximo(double custoMaximo) {
+        this.custoMaximo = custoMaximo;
+    }
+
+    public double getPerdasCombate() {
+        return perdasCombate;
+    }
+
+    public void setPerdasCombate(double perdasCombate) {
+        this.perdasCombate = perdasCombate;
+    }
+
+    public double getNumeroCombates() {
+        return numeroCombates;
+    }
+
+    public void setNumeroCombates(double numeroCombates) {
+        this.numeroCombates = numeroCombates;
+    }
+    
+    
+    
+}
